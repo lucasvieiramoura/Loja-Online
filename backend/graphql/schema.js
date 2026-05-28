c7d850b6c2f655732e43eba5698c0144ba1d3eab
Lucas Vieira <lucasvieiramoura@gmail.com>
2026-05-28 16:30:18 -0300

    Criação do Product Model && Schema Graphql

---
+++ backend/graphql/schema.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+const Product = require('../models/Product');
+
+// 1. Definição dos Tipos
+const typeDefs = gql`
+    type Product {
+        id: ID!
+        name: String!
+        description: String
+        price: Float!
+        stock: Int!
+        category: String
+        imageUrl: String
+        createdAt: String
+        updatedAt: String
+    }
+        type Query {
+        getProducts: [Product]
+        getProduct(id: ID!): Product
+    }
+
+    type Mutation {
+        createProduct(name: String!, description: String, price: Float!, stock: Int!, category: String, imageUrl: String): Product
+        updateProduct(id: ID!, name: String, description: String, price: Float, stock: Int, category: String, imageUrl: String): Product
+        deleteProduct(id: ID!): Boolean
+    }
+`;
+
+// 2. Implementação dos Resolvers
+const resolvers = {
+    Query: {
+        getProducts: async () => await Product.find(),
+        getProduct: async (_, { id }) => await Product.findById(id),
+    },
+    Mutation: {
+        createProduct: async (_, { id, ...updateData }) => {
+            const product = new Product(updateData);
+            return await product.save();
+        },
+        updateProduct: async (_, { id, ...updateData }) => {
+            return await Product.findByIdAndUpdate(id, updateData, { new: true });
+        },
+        deleteProduct: async (_, { id }) => {
+            await Product.findByIdAndDelete(id);
+            return "Produto deletado com sucesso!";
+        }
+    }
+};
+
+module.exports = { typeDefs, resolvers };
