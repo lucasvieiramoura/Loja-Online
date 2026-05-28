061538e212ec63ca3d25d90a432654e5e1926fad
Lucas Vieira <lucasvieiramoura@gmail.com>
2026-05-28 16:33:55 -0300

    Configurando servidor para conectar com MongoDB e usar o GraphQL ApolloServer

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { typeDefs, resolvers } = require('./graphql/schema');
+
+async function startServer() {
+    const app = express();
+    app.use(cors());
+
+    // Conexão Mongo DB
+    const monogoURI = 'mongodb://localhost:27017/loja-online';
+    await mongoose.connect(monogoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB conectado com sucesso!');
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();
+    server.applyMiddleware({ app });
+
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}${server.graphqlPath}`);
+    });
+}
+
+startServer();
